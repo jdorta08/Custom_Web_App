4e077eb033d47f3ce2edfaa84ff00e8dd5a1b9c0
jdorta08 <49850121+jdorta08@users.noreply.github.com>
2021-03-12 07:07:59 -0500

    new update with new form steps

--- design.js
+++ design.js
@@ -1,4 +1,4 @@
-var currentTab = 0; // Current tab is set to be the first tab (0)
+/*var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n) {
@@ -69,5 +69,5 @@ function fixStepIndicator(n) {
     }
     //... and adds the "active" class to the current step:
     x[n].className += " active";
-}
+}*/
 
